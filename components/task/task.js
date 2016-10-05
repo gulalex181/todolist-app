bda2fafe5600f743ff6164094fc1811e049c39ce
gulalex181 <gulalex181@gmail.com>
2016-10-05 18:28:08 +0300

    task remove

--- components/task/task.js
+++ components/task/task.js
@@ -7,6 +7,7 @@
 			this._data = options.data;
 
 			this._render();
+			this._initEvents();
 		}
 
 		_render () {
@@ -17,29 +18,40 @@
 			this._elem.append(this._list);
 
 			this._data.forEach( item => {
-				let li = document.createElement('li');
-				li.className = 'task__item';
-				li.textContent = item.anchor;
-				this._list.append(li);
+				this._renderItem(item.anchor);
 			});
 		}
 
+		_renderItem (text) {
+			let li = document.createElement('li');
+			li.className = 'task__item';
+			li.textContent = text;
+			this._list.append(li);
+
+			let i = document.createElement('i');
+			i.className = 'task__delete';
+			i.innerHTML = '&#128473;';
+			li.append(i);
+		}
+
+		_initEvents () {
+			this._list.addEventListener('click', this._deleteTask.bind(this));
+		}
+
+		_deleteTask (event) {
+			let target = event.target;
+			let i = target.closest('i');
+			if (!i || !this._list.contains(i)) return;
+
+			let li = i.closest('li');
+			if (!li || !this._list.contains(li)) return;
+
+			li.remove();
+		}
+
 		addTask (task) {
-			let li = document.createElement('li');
-			li.className = 'task__item';
-			li.textContent = task;
-
-			this._list.append(li);
+			this._renderItem(task);
 		}
-
-		deleteTask (task) {
-			let li = document.createElement('li');
-			li.className = 'task__item';
-			li.textContent = task;
-
-			this._list.append(li);
-		}
-
 	}
 
 
